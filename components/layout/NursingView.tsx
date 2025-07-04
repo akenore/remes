29b36ae4613d80a835171ded008389bba39cc6f9
Akenore <joudakenore@gmail.com>
2025-07-04 08:44:53 +0100

    update

--- components/layout/NursingView.tsx
+++ components/layout/NursingView.tsx
@@ -1,11 +1,11 @@
-import HeroPage from "../ui/HearoPage";
+import Hero2 from "../ui/Hero2";
 import Footer from "../ui/Footer";
 import ContactForm from "../ui/ContactForm";
 
 export default function NursingView() {
      return (
           <>
-               <HeroPage />
+               <Hero2 />
                <main className="pt-96">
                <ContactForm />
                </main>
--- components/ui/Hero2.tsx
+++ components/ui/Hero2.tsx
@@ -4,21 +4,20 @@ import Card from './Card';
 import Navbar from './Navbar';
 
 
-export default function HeroPage() {
+export default function Hero2() {
      const t = useTranslations();
      const locale = useLocale();
 
      return (
-          <div className="w-full h-[763px] bg-cover bg-top bg-[url('/hero-2/bg-mobile.jpg')] sm:bg-[url('/hero-2/bg-desktop.jpg')] bg-no-repeat">
+          <div className="w-full h-[990px] bg-cover bg-top bg-[url('/hero-2/bg-mobile.jpg')] sm:bg-[url('/hero-2/bg-desktop.jpg')] bg-no-repeat">
                <Navbar />
-               <div className='relative z-10 flex flex-col items-start justify-start w-full max-w-4xl mx-auto text-center md:text-left gap-6 px-4 '>
-                    <div className='mb-20 max-h-80 sm:max-h-96 md:max-h-[28rem]'>
+               <div className='relative z-10 flex flex-col items-start justify-start w-full max-w-xl mx-auto text-center md:text-left gap-6 px-4'>
+                    <div className='mb-20 max-h-80 sm:max-h-96 md:max-h-[28rem] md:-mt-20 lg:-mt-32 xl:-ml-80'>
                          <div className="h-64 flex flex-col justify-center md:justify-start">
-                              <h1 className="px-6 md:px-0 text-[2rem] md:text-[3.875rem] mb-6 text-gold leading-tight font-myanmar">
-                                   Maison
-                                   de Repos
+                              <h1 className="px-6 md:px-0 text-[2rem] md:text-[3.875rem] mb-6 text-gold leading-tight font-myanmar max-w-sm sm:max-w-xl">
+                                   Maison de Repos
                               </h1>
-                              <p className="px-6 md:px-0 text-white text-[1rem] md:text-[1.2rem] mb-8 drop-shadow-lg">
+                              <p className="px-6 md:px-0 text-white text-[1rem] md:text-[1.2rem] mb-8 drop-shadow-lg max-w-sm sm:max-w-xl">
                                    A cat named Mittens has made national headlines after she managed to find her way back home, despite being lost for over a week. Mittens
                               </p>
                          </div>
